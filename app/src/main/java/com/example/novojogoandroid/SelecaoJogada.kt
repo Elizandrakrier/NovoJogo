5a242156b9a818858a1e48000527eb451207122c
Elizandra Krier <elizandrarpk@gmail.com>
2024-04-28 19:18:40 -0300

    [WIP] Incluindo Spinner
    Inclui o Spinner com as 3 opções propostas

--- app/src/main/java/com/example/novojogoandroid/SelecaoJogada.kt
+++ app/src/main/java/com/example/novojogoandroid/SelecaoJogada.kt
@@ -1,6 +1,11 @@
 package com.example.novojogoandroid
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.novojogoandroid.databinding.SelecaoJogadaBinding
 
@@ -11,6 +16,26 @@ class SelecaoJogada : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val spinner = binding.spinnerSelecao
+        val listIntem = listOf("Pedra", "Tesoura", "Papel")
+
+        val arrayAdapter = ArrayAdapter(
+            this, com.google.android.material.R.layout.support_simple_spinner_dropdown_item, listIntem)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = arrayAdapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                Toast.makeText(this@SelecaoJogada, "Você selecionou $selectedItem",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+
+        }
 
     }
 }
